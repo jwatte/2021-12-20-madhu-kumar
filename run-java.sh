1eb8693eceb2c17580e7a6b61a194619b443d562
Jon Watte <jwatte@observeinc.com>
2021-11-23 02:58:46 +0000

    First Commit

---
+++ run-java.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -o pipefail
+set -e
+set -x
+
+JAVAC=javac
+JAVA=java
+
+trap 'rm -f /tmp/test*.csv SpreadCalc SpreadCalc.class' EXIT
+
+$JAVAC SpreadCalc.java
+
+$JAVA SpreadCalc < provided/testinput1.csv > /tmp/testoutput1.csv
+diff -u /tmp/testoutput1.csv provided/testoutput1.csv
+
+$JAVA SpreadCalc  < provided/testinput2.csv > /tmp/testoutput2.csv
+diff -u /tmp/testoutput2.csv provided/testoutput2.csv
+
+$JAVA SpreadCalc < provided/testinput3.csv > /tmp/testoutput3.csv
+diff -u /tmp/testoutput3.csv provided/testoutput3.csv
+
+echo 'success!'
+
